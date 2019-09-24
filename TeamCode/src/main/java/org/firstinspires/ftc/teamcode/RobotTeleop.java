69067129acb8e7332e4092e0a421a9b1b2d7b31f
Alex71827 <44625347+Alex71827@users.noreply.github.com>
2019-09-24 18:58:28 -0500

    Update RobotTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleop.java
@@ -104,6 +104,7 @@ public class RobotTeleop extends OpMode
         // Setup a variable for each drive wheel to save power level for telemetry
         double leftPower;
         double rightPower;
+        double centerPower;
 
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
@@ -111,9 +112,11 @@ public class RobotTeleop extends OpMode
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
         double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
+        double turn  =  gamepad1.left_stick_x;
+        double enable_h= gamepad1.right_stick_x;
         leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
         rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        centerPower   = Range.clip(enable_h + drive, -1.0, 1.0) ;
 
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
@@ -126,7 +129,7 @@ public class RobotTeleop extends OpMode
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Motors", "left (%.2f), right (%.2f), center  (%.2f)", leftPower, rightPower, centerPower);
     }
 
     /*
